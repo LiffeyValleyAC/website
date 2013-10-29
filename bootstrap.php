414fabadf2589c06246bf2555b9116b3b15bf226
Mike Griffin <mgriffin@museum.ie>
2013-10-29 15:14:49 +0000

    Adds twig for layout and bootstrap for css

--- bootstrap.php
+++ bootstrap.php
@@ -5,8 +5,16 @@ require 'vendor/autoload.php';
 $app = new Silex\Application();
 $app['debug'] = true;
 
-$app->get('/', function () {
-    return "Hi LVAC people";
+$app->register(
+    new Silex\Provider\TwigServiceProvider(),
+    array(
+        'twig.path' => __DIR__.'/templates',
+        'debug' => true
+    )
+);
+
+$app->get('/', function () use ($app) {
+    return $app['twig']->render('index.html');
 });
 
 $app->run();
