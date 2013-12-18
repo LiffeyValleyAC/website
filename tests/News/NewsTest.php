a50785474e08e42e3e55ca5bc0d631c2a1fb4f68
Mike Griffin <mgriffin@museum.ie>
2013-12-18 11:06:45 +0000

    Adds tests for the save news function

--- tests/News/NewsTest.php
+++ tests/News/NewsTest.php
@@ -1,6 +1,8 @@
 <?php
 namespace LVAC\NewsTest;
 
+use \Mockery as m;
+
 class NewsTest extends \PHPUnit_Framework_TestCase
 {
     protected $db;
@@ -10,11 +12,44 @@ class NewsTest extends \PHPUnit_Framework_TestCase
             'sqlite:LVAC.sqlite'
         );
     }
+
     public function testGetNewsReturnsTenNewsItems()
     {
         $news = new \LVAC\News\NewsMapper($this->db);
         $result = $news->getNews();
 
         $this->assertEquals(10, count($result));
+    }
+
+    public function testSaveWorksWithMocking()
+    {
+        $mock = m::mock('StdClass');
+        $mock->shouldReceive('execute')->once()->andReturn(true);
+
+        $mock_db = m::mock('PDOMock');
+        $mock_db->shouldReceive('prepare')->once()->andReturn($mock);
+
+        $news_model = new \LVAC\News\News();
+
+        $news = new \LVAC\News\NewsMapper($mock_db);
+        $saved_news = $news->save($news_model);
+
+        $this->assertNotEquals(false, $saved_news);
+    }
+
+    public function testSaveReturnsFalseIfThereIsAProblemWithSaving()
+    {
+        $mock = m::mock('StdClass');
+        $mock->shouldReceive('execute')->once()->andReturn(false);
+
+        $mock_db = m::mock('PDOMock');
+        $mock_db->shouldReceive('prepare')->once()->andReturn($mock);
+
+        $news_model = new \LVAC\News\News();
+
+        $news = new \LVAC\News\NewsMapper($mock_db);
+        $saved_news = $news->save($news_model);
+
+        $this->assertFalse($saved_news);
     }
 }
