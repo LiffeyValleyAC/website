c27ceaa359200ee035d055e53f2b32630756073a
Mike Griffin <griffinm@gmail.com>
2014-09-07 00:05:20 +0100

    Updates the test database creation script

--- seed/db_setup.php
+++ seed/db_setup.php
@@ -15,7 +15,7 @@ $db->exec("DROP TABLE IF EXISTS news");
 $db->exec("CREATE TABLE news (id INTEGER PRIMARY KEY ASC, title TEXT, body TEXT, date, slug TEXT, location TEXT)");
 
 $file = APP_ROOT . 'seed/LVACEvent.txt';
-$news_mapper = new \LVAC\News\NewsMapper($db);
+$news_mapper = new \LVAC\News\Mapper($db);
 
 $fp = fopen($file, 'r');
 $data = fgetcsv($fp, 0, '|');
@@ -36,3 +36,13 @@ $db->exec("DROP TABLE IF EXISTS users");
 $db->exec("CREATE TABLE users (id INTEGER PRIMARY KEY ASC, email TEXT, password TEXT, name TEXT, nickname TEXT)");
 
 $db->exec("INSERT INTO users (id, email, password, name, nickname) VALUES (1, 'fortest@test.fake', '$2y$10\$IYJpRpG3KrBOmmomSPQ9ieVTGQfkjU28iSBwRkZvOAmLXgioIjbxK', 'test', 'fortest')");
+
+$db->exec("DROP TABLE IF EXISTS training");
+$db->exec("CREATE TABLE training (id INTEGER PRIMARY KEY ASC, user_id INTEGER, date, title TEXT, description TEXT)");
+
+$db->exec("INSERT INTO training (id, user_id, date, title, description) VALUES (1, 1, '2014-02-01 17:00', '8x 200m', 'Cold and wet. Tough session')");
+
+$db->exec("DROP TABLE IF EXISTS races");
+$db->exec("CREATE TABLE races (id INTEGER PRIMARY KEY ASC, title TEXT, date)");
+
+$db->exec("INSERT INTO races (id, title, date) VALUES (1, 'Myles Cullen 10k', '2014-09-15 11:30')");
