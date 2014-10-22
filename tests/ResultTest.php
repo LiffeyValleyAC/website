de9fb07753e302fa36fb0b1de99a9cd2996aef21
Mike Griffin <griffinm@gmail.com>
2014-10-22 07:48:57 +0100

    Adds more tests to the Result\Result class

--- tests/ResultTest.php
+++ tests/ResultTest.php
@@ -14,4 +14,46 @@ class ResultTest extends \PHPUnit_Framework_TestCase
 
         $this->assertInstanceOf('DateInterval', $result);
     }
+
+    public function testGetHandicapReturnsGuest()
+    {
+        $model = new \LVAC\Result\Result();
+        $model->setHandicap('guest');
+
+        $result = $model->getHandicap();
+
+        $this->assertEquals('guest', $result);
+    }
+
+    public function testGetHandicapReturnsADateInterval()
+    {
+        $model = new \LVAC\Result\Result();
+        $model->setHandicap('PT1M4S');
+
+        $result = $model->getHandicap();
+
+        $this->assertNotEquals('guest', $result);
+        $this->assertInstanceOf('DateInterval', $result);
+    }
+
+    public function testGetNettReturnsBlankForAGuest()
+    {
+        $model = new \LVAC\Result\Result();
+        $model->setHandicap('guest');
+
+        $result = $model->getNett();
+
+        $this->assertEquals('', $result);
+    }
+
+    public function testGetNettReturnsTheDifferenceBetweenDurationAndHandicap()
+    {
+        $model = new \LVAC\Result\Result();
+        $model->setDuration('PT10M4S');
+        $model->setHandicap('PT1M4S');
+
+        $result = $model->getNett();
+
+        $this->assertEquals('09m 00s', $result);
+    }
 }
